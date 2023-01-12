c5de0eef8744d866450e15c6398d10e4110e1611
Karthik Belida <55057144+karthik0309@users.noreply.github.com>
2023-01-12 15:21:57 +0530

    Update StockData.js

--- controllers/StockData.js
+++ controllers/StockData.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const MARCKET_STACK_API = process.env.MARCKET_STACK_API
 const ACCESS_KEY = process.env.ACCESS_KEY
 
+console.log(MARCKET_STACK_API)
 exports.getAllStock = async(req,res)=>{
     try{
         const response = await axios.get(`${MARCKET_STACK_API}/v1/tickers?access_key=${ACCESS_KEY}`)  
